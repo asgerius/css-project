20f08feb5ee3e6e7c9ab757529276110bad366ea
Asger Schultz <asger.s@protonmail.com>
2021-05-02 21:11:35 +0200

    Hjemmesidearbejde

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -3,19 +3,32 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { PlotlyModule } from 'angular-plotly.js';
+import * as PlotlyJS from 'plotly.js-dist';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TfidfComponent } from './tfidf/tfidf.component';
+import { DataComponent } from './data/data.component';
+import { GraphComponent } from './graph/graph.component';
+import { SentimentComponent } from './sentiment/sentiment.component';
+
+PlotlyModule.plotlyjs = PlotlyJS;
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TfidfComponent,
+    DataComponent,
+    GraphComponent,
+    SentimentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    PlotlyModule,
   ],
   providers: [DecimalPipe],
   bootstrap: [AppComponent]
---
+++ website/src/app/tfidf/tfidf.component.ts
@@ -0,0 +1,66 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as Plotly from 'plotly.js';
+import { CommonService, layoutDefaults } from '../common/common.service';
+
+@Component({
+  selector: 'app-tfidf',
+  templateUrl: './tfidf.component.html',
+  styleUrls: ['./tfidf.component.scss']
+})
+export class TfidfComponent implements OnInit {
+
+  classifyText: string = "";
+  cleanedText: string = "";
+
+  public validation: any = null;
+
+  constructor(public commonService: CommonService) { }
+
+  ngOnInit() {
+    this.validation = {
+      data: [
+        {
+          x: this.commonService.validation?.number_of_words,
+          y: this.commonService.validation?.accs.map(x => 100*x),
+          name: "Accuracy",
+        },
+        {
+          x: this.commonService.validation?.number_of_words,
+          y: this.commonService.validation?.f1s.map(x => 100*x),
+          name: "Weighted F1",
+        },
+      ],
+      layout: {
+        xaxis: {
+          title: "Maximum vocabulary size",
+          type: 'log',
+          autorange: true
+        },
+        yaxis: {
+          title: "Validation score [%]",
+        },
+        title: "Classifier validation",
+      },
+      config: {
+        responsive: true,
+        scrollZoom: true,
+      },
+    };
+  }
+
+  private clean(text: string): string {
+    text = text.replace(/\bhttp\S+/gm, "");
+    text = text.replace(this.commonService.stopwordRegex, "");
+    text = text.replace(/<.*?>/gm, "");
+    text = text.replace(/[^a-zA-Z0-9\s]/gm, "");
+    text = text.replace(/\s/gm, " ");
+    return text.toLowerCase();
+  }
+
+  public updateClassification(input: string) {
+    this.classifyText = input;
+    this.cleanedText = this.clean(this.classifyText);
+    this.commonService.classify(this.cleanedText);
+  }
+
+}
